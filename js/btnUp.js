cd095897a1e80471ae491b4cfd20c90112e9c489
Santiago Leal <santimleal400@gmail.com>
2022-03-09 20:29:14 -0300

    Nuevas Funciones WP y subir

---
+++ js/btnUp.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){ 
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 0){
+            $('#up').slideDown(300);
+
+            } else {
+                $('#up').slideUp(300);
+            }
+
+        });
+
+        $('#up').on('click',function(){
+            $('body, html').animate({
+                scrollTop:0
+            }, 700);
+            return false;
+        });
+
+    });
+
